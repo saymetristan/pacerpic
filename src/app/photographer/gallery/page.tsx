3f38175097a92686f62e01e33ae0a467de6e14e7
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2025-01-16 23:32:58 -0600

    filtros galeria v2

--- src/app/photographer/gallery/page.tsx
+++ src/app/photographer/gallery/page.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import { GalleryHeader } from "@/components/gallery/gallery-header";
 import { GalleryGrid } from "@/components/gallery/gallery-grid";
 import { GalleryFilters } from "@/components/gallery/gallery-filters";
+import { useState } from "react";
 
 export default function GalleryPage() {
+  const [filters, setFilters] = useState({
+    status: "all",
+    events: [] as string[],
+    tags: [] as string[]
+  });
+
   return (
     <div className="p-8 space-y-8">
       <GalleryHeader />
       <div className="flex flex-col lg:flex-row gap-8">
         <div className="w-full lg:w-64 flex-none">
-          <GalleryFilters />
+          <GalleryFilters onFilterChange={setFilters} />
         </div>
         <div className="flex-1">
-          <GalleryGrid />
+          <GalleryGrid filters={filters} />
         </div>
       </div>
     </div>
